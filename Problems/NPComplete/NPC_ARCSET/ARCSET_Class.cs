b1116ad3b7eda72ad1cc2d23214f75d4e3c3a347
Alex <dandiviney@isu.edu>
2022-01-31 11:27:41 -0700

    changed some variable names

--- Problems/NPComplete/NPC_ARCSET/ARCSET_Class.cs
+++ Problems/NPComplete/NPC_ARCSET/ARCSET_Class.cs
@@ -16,7 +16,7 @@ class ARCSET : IProblem<GenericSolver,GenericVerifier>{
 
     //ALEX NOTE: The standard mathematical form for a DIGRAPH is A = { x,y,z} r = {(x,y),(y,z),(z,x)} where A is a set of nodes and r is a set of pairs of edges. (r stands for relation)
     private string _defaultInstance = "A = {1,2,3,4} r = {(4,1),(1,2),(4,3),(3,2),(2,4)}";
-    private string _phi = string.Empty; 
+    private string _arcset = string.Empty; 
     private GenericSolver _defaultSolver = new GenericSolver();
     private GenericVerifier _defaultVerifier = new GenericVerifier(); //Verifier needs to implement a Depth First Search. 
 
@@ -47,12 +47,12 @@ class ARCSET : IProblem<GenericSolver,GenericVerifier>{
             return _defaultInstance;
         }
     }
-    public string phi {
+    public string arcset {
         get {
-            return _phi;
+            return _arcset;
         }
         set {
-            _phi = value;
+            _arcset = value;
         }
     }
     public GenericSolver defaultSolver {
@@ -69,13 +69,13 @@ class ARCSET : IProblem<GenericSolver,GenericVerifier>{
 
     // --- Methods Including Constructors ---
     public ARCSET() {
-        _phi = defaultInstance;
+        _arcset = defaultInstance;
     }
-    public ARCSET(string phiInput) {
-        _phi = phiInput;
+    public ARCSET(string arcInput) {
+        _arcset = arcInput;
     }
 
-    public void ParseProblem(string phiInput) {
+    public void ParseProblem(string arcInput) {
 
     }
 }
