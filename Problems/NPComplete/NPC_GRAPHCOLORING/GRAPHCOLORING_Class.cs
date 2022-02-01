4054fa9ae2f1d39e8eaead4a395474e895fa4544
igbodani <igbodani@isu.edu>
2022-02-01 00:57:48 -0700

    added vscode gitIgnore

--- Problems/NPComplete/NPC_GRAPHCOLORING/GRAPHCOLORING_Class.cs
+++ Problems/NPComplete/NPC_GRAPHCOLORING/GRAPHCOLORING_Class.cs
@@ -10,12 +10,12 @@ class GRAPHCOLORING : IProblem<GenericSolver, GenericVerifier>{
     // ---- Fields  -----
 
     private string _problemName = "GRAPHCOLORING";
-    private string _formalDefinition = "<G,k> | G is a graph that has a k-coloring";
-    private string _problemDefinition = "Vertex coloring problem is defined as given m colors,find a way of coloring the vertices of a graph such that no two adjacent vertices are colored using the same color";
+    private string _formalDefinition = "{<G,k> | G is a graph that has a k-coloring}";
+    private string _problemDefinition = "Vertex coloring problem is defined as given k colors, find a way of coloring the vertices of a graph such that no two adjacent vertices are colored using the same color";
 
 
     private string _source = "Karp, Richard M. Reducibility among combinatorial problems. Complexity of computer computations. Springer, Boston, MA, 1972. 85-103.";
-    private string _defaultInstance = "{x1,x2,x3,x4}";
+    private string _defaultInstance = "( ( {a,b,c,d,e}, { {a,b}, {b,a}, {b,c} }), 3)";
 
     private string _Gk =  string.Empty;
 
