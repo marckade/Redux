3d5094775f2ad88eb68f7dec8b1c70926002916b
eardcale <eardcale@isu.edu>
2022-02-19 00:45:07 -0700

    0-1 Integer Programming Verifier

--- Problems/NPComplete/NPC_INTPROGRAMMING0_1/Verifiers/GenericVerifier.cs
+++ Problems/NPComplete/NPC_INTPROGRAMMING0_1/Verifiers/GenericVerifier.cs
@@ -30,4 +30,35 @@ class GenericVerifier : IVerifier {
     public GenericVerifier() {
         
     }
+    public List<int> parseCertificate(string certificate){
+        List<int> c = new List<int>();
+        string[] stringVector = certificate.Replace("(","").Replace(")","").Split(" ");
+        for(int i=0; i<stringVector.Length; i++){
+            c.Add(int.Parse(stringVector[i]));
+        }
+        return c;
+    }
+    public bool Verify(INTPROGRAMMING0_1 Problem, string c){
+        List<int> certificate = parseCertificate(c);
+        
+        //checks that the certificate is the correct size
+        if(certificate.Count != Problem.C[0].Count){return false;}
+
+        //compute C*certificate, or Cx
+        List<int> solution = new List<int>();
+        foreach(var row in Problem.C){
+            int value = 0;
+            for(int i = 0; i< row.Count; i++){
+                value += row[i]*certificate[i];
+            }
+            solution.Add(value);
+        }
+
+        //checks that C*solution <= d
+        for(int i=0; i<Problem.d.Count; i++){
+            if(!(solution[i] <= Problem.d[i])){return false;}
+        }
+
+        return true; 
+    }
 }
