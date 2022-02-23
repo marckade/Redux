04adc119e38ffaff4b5da740d2356a81fd7e5f63
Alex <dandiviney@isu.edu>
2022-02-22 20:16:53 -0700

    fixed issue with reduction, now reduces from VERTEXCOVER instead of ExactCover

--- Problems/NPComplete/NPC_ARCSET/ReduceTo/NPC_CLIQUE/NCOV_TO_ARCSET.cs
+++ Problems/NPComplete/NPC_ARCSET/ReduceTo/NPC_CLIQUE/NCOV_TO_ARCSET.cs
@@ -1,16 +1,18 @@
 using API.Interfaces;
 using API.Problems.NPComplete.NPC_ARCSET;
+using API.Problems.NPComplete.NPC_VERTEXCOVER;
+
 
 namespace API.Problems.NPComplete.NPC_ExactCover.ReduceTo.NPC_ARCSET;
 
-class NCOV_TO_ARCSETReduction : IReduction<ExactCover, ARCSET> {
+class NCOV_TO_ARCSETReduction : IReduction<VERTEXCOVER, ARCSET> {
 
   
 
     // --- Fields ---
     private string _reductionDefinition = "This Reduction is an implementation of Lawler and Karp's reduction as laid out in Karp's 21 NP_Complete Problems.";
     private string _source = "http://cgi.di.uoa.gr/~sgk/teaching/grad/handouts/karp.pdf"; //Alex NOTE: Change later to real citation.
-    private ExactCover _reductionFrom;
+    private VERTEXCOVER _reductionFrom;
     private ARCSET _reductionTo;
 
 
@@ -25,7 +27,7 @@ class NCOV_TO_ARCSETReduction : IReduction<ExactCover, ARCSET> {
             return _source;
         }
     }
-    public ExactCover reductionFrom {
+    public VERTEXCOVER reductionFrom {
         get {
             return _reductionFrom;
         }
@@ -46,19 +48,21 @@ class NCOV_TO_ARCSETReduction : IReduction<ExactCover, ARCSET> {
 
     public NCOV_TO_ARCSETReduction(){
 
-        _reductionFrom = new ExactCover();
+        _reductionFrom = new VERTEXCOVER();
         _reductionTo = new ARCSET();
-
     }
-    public NCOV_TO_ARCSETReduction(ExactCover from, ARCSET to) {
-        _reductionFrom = from;
-        //_reduceTo = reduce()
+    public NCOV_TO_ARCSETReduction(VERTEXCOVER from, ARCSET to) {
+         _reductionFrom = from;
+        _reductionTo = reduce();
         
     }
     public ARCSET reduce() {
-       
+        UndirectedGraph ug = new UndirectedGraph(_reductionFrom.Gk);
+         string dgString = ug.reduction();
+        //DirectedGraph dg = new DirectedGraph(dgString);
+        ARCSET arcset = new ARCSET(dgString);
         
-        return new ARCSET();
+        return arcset;
     }
 }
 // return an instance of what you are reducing to
