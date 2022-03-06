72930b9eeda1b6221122808d4e21ab5706135a70
igbodani <igbodani@isu.edu>
2022-03-06 00:50:59 -0700

    pulling from current

--- Problems/NPComplete/NPC_GRAPHCOLORING/Verifiers/GenericVerifier.cs
+++ Problems/NPComplete/NPC_GRAPHCOLORING/Verifiers/GenericVerifier.cs
@@ -3,30 +3,37 @@ using API.Problems.NPComplete.NPC_GRAPHCOLORING;
 
 namespace API.Problems.NPComplete.NPC_GRAPHCOLORING.Verifiers;
 
-class GenericVerifier : IVerifier {
+class GenericVerifier : IVerifier
+{
 
-    
 
-    # region Fields
+
+    #region Fields
     private string _verifierName = "Generic Verifier";
     private string _verifierDefinition = "This is a verifier for GRAPHCOLORING";
     private string _source = " ";
 
-    # endregion
-   
+    #endregion
+
     #region Properties
-      public string verifierName {
-        get {
+    public string verifierName
+    {
+        get
+        {
             return _verifierName;
         }
     }
-    public string verifierDefinition {
-        get {
+    public string verifierDefinition
+    {
+        get
+        {
             return _verifierDefinition;
         }
     }
-    public string source {
-        get {
+    public string source
+    {
+        get
+        {
             return _source;
         }
     }
@@ -35,15 +42,17 @@ class GenericVerifier : IVerifier {
     #endregion 
 
     #region Constructors
-    public GenericVerifier() {
-        
+    public GenericVerifier()
+    {
+
     }
 
     #endregion
 
     #region Methods
 
-    public void verify(){
+    public void verify()
+    {
 
     }
 
@@ -56,13 +65,15 @@ class GenericVerifier : IVerifier {
 
 
     */
-    public bool DFS(GRAPHCOLORING g, int v){
+    public bool DFS(GRAPHCOLORING g)
+    {
 
         bool[] visited = new bool[g.nodes.Count];
         string node = g.nodes[0];
 
         //Checks if node has a color in k
-        if(g.validColor(g.getNodeColor(node))) {
+        if (g.validColor(g.getNodeColor(node)))
+        {
 
             return checkNeighbors(g, 0, g.getNodeColor(node), visited);
         }
@@ -77,36 +88,69 @@ class GenericVerifier : IVerifier {
       Checks if connected adjacent have a different colors, and if 
       the color is in K
     */
-    private bool checkNeighbors(GRAPHCOLORING g, int v, string color,  bool[] visited){
+    private bool checkNeighbors(GRAPHCOLORING g, int v, string color, bool[] visited)
+    {
 
-       visited[v] = true;
+        visited[v] = true;
 
-       List<string> list = g.getAdjNodes(g.nodes[v]);
+        List<string> list = g.getAdjNodes(g.nodes[v]);
 
-       for(int i = 0; i< list.Count; i++){
-           string newColor = g.getNodeColor(list[i].ToLower());
-            
+        for (int i = 0; i < list.Count; i++)
+        {
+            string newColor = g.getNodeColor(list[i].ToLower());
+
             //  checks if node has been visited, 
-           if(!visited[i] ) {
+            if (!visited[i])
+            {
 
                 //checks if node has the same color as an adjacent node,
                 //checks if node has a color in k
-               if(!(newColor.Equals(color.ToLower()))  && (g.validColor(newColor))){
+                if (!(newColor.Equals(color.ToLower())) && (g.validColor(newColor)))
+                {
 
                     return checkNeighbors(g, i, newColor, visited);
 
-               }else{
+                }
+                else
+                {
 
-                   return false;
-               }
+                    return false;
+                }
             }
-       }
-       return false;
+        }
+        return false;
+    }
+
+
+
+    private Dictionary<string, string> parseCertificate(string certificate)
+    {
+
+        Dictionary<string, string> nodeColoring = new Dictionary<string, string>();
+        
+
+        if (certificate.Length != 0)
+        {
+            string[] nodes = certificate.Split(',');
+
+            foreach (string node in nodes)
+            {
+
+                string [] nodeColor  = node.Split(':');
+               
+                    
+                nodeColoring.Add(nodeColor[0], nodeColor[1]);
+                
+            }
+
+        }
+
+        return nodeColoring;
     }
 
     #endregion
-  
+
 
     // --- Methods Including Constructors ---
- 
+
 }
