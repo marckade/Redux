b537721a3bd52bb121d4c518cf08567d7a1a7e0d
eardcale <eardcale@isu.edu>
2022-03-06 19:33:50 -0700

    Verifier Controller

--- Problems/NPComplete/NPC_INTPROGRAMMING0_1/INTEGERPROGRAMMING0_1_Controller.cs
+++ Problems/NPComplete/NPC_INTPROGRAMMING0_1/INTEGERPROGRAMMING0_1_Controller.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using API.Problems.NPComplete.NPC_INTPROGRAMMING0_1;
-using API.Problems.NPComplete.NPC_CLIQUE;
-using API.Problems.NPComplete.NPC_SAT3.ReduceTo.NPC_CLIQUE;
+using API.Problems.NPComplete.NPC_INTPROGRAMMING0_1.Verifiers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -26,6 +25,24 @@ public class INTPROGRAMMING0_1GenericController : ControllerBase {
         return jsonString;
     }
 
+[ApiController]
+[Route("[controller]")]
+public class GenericVerifier0_1INTPController : ControllerBase {
+
+    [HttpGet]
+    public String getInstance([FromQuery]string certificate, [FromQuery]string problemInstance) {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        INTPROGRAMMING0_1 INTPROGRAMMING0_1_Problem = new INTPROGRAMMING0_1(problemInstance);
+        GenericVerifier0_1INTP verifier = new GenericVerifier0_1INTP();
+
+        Boolean response = verifier.verify(INTPROGRAMMING0_1_Problem,certificate);
+        // Send back to API user
+        string jsonString = JsonSerializer.Serialize(response.ToString(), options);
+        return jsonString;
+    }
+
+}
+    
 
 }
 
