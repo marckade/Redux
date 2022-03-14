a9efd165b969ce29dad7d239553e43d4c7a707cb
aamijani <55214847+aamijani@users.noreply.github.com>
2022-03-13 21:29:05 -0600

    Added VC Verifier and Solver

--- Problems/NPComplete/NPC_VERTEXCOVER/Solvers/GenericSolver.cs
+++ Problems/NPComplete/NPC_VERTEXCOVER/Solvers/GenericSolver.cs
@@ -1,7 +1,9 @@
 using API.Interfaces;
 
+
+
 namespace API.Problems.NPComplete.NPC_VERTEXCOVER.Solvers;
-class GenericSolver : ISolver {
+class VCSolverJanita : ISolver {
 
     // --- Fields ---
     private string _solverName = "Generic Solver";
@@ -25,7 +27,64 @@ class GenericSolver : ISolver {
         }
     }
     // --- Methods Including Constructors ---
-    public GenericSolver() {
+    public VCSolverJanita() {
 
+    }
+
+
+    public List<KeyValuePair<string, string>> Solve(String G){
+        //{{a,b,c,d,e,f,g} : {(a,b) & (a,c) & (c,d) & (c,e) & (d,f) & (e,f) & (e,g)}}
+        List<KeyValuePair<string, string>> edges = getEdges(G);
+        List<KeyValuePair<string, string>> C = new List<KeyValuePair<string, string>>();
+        Random rnd = new Random();
+
+        while (edges.Count > 0){
+            int index = rnd.Next(edges.Count);
+            KeyValuePair<string, string> edge = edges[index];
+            KeyValuePair<string,string> fullEdge = new KeyValuePair<string,string>(edge.Key, edge.Value);
+            C.Add(fullEdge);
+            foreach (KeyValuePair<string, string> e in new List<KeyValuePair<string, string>>(edges)){
+                if (e.Key.Equals(edge.Key)){
+                    KeyValuePair<string,string> rmEdge = new KeyValuePair<string,string>(edge.Key, edge.Value);
+                    edges.Remove(rmEdge);
+                }
+                if (e.Key.Equals(edge.Value)){
+                    KeyValuePair<string,string> rmEdge = new KeyValuePair<string,string>(edge.Key, edge.Value);
+                    edges.Remove(rmEdge);
+                }
+                if (e.Value.Equals(edge.Key)){
+                    KeyValuePair<string,string> rmEdge = new KeyValuePair<string,string>(edge.Key, edge.Value);
+                    edges.Remove(rmEdge);
+                }
+                if (e.Value.Equals(edge.Value)){
+                    KeyValuePair<string,string> rmEdge = new KeyValuePair<string,string>(edge.Key, edge.Value);
+                    edges.Remove(rmEdge);
+                }
+            }
+        }
+        return C; 
+
+    }
+
+    private static List<KeyValuePair<string, string>> getEdges(string Ginput) {
+
+        List<KeyValuePair<string, string>> allGEdges = new List<KeyValuePair<string, string>>();
+
+        string strippedInput = Ginput.Replace("{", "").Replace("}", "").Replace(" ", "").Replace("(", "").Replace(")","");
+        
+        // [0] is nodes,  [1] is edges,  [2] is k.
+        string[] Gsections = strippedInput.Split(':');
+        string[] Gedges = Gsections[1].Split('&');
+        
+        foreach (string edge in Gedges) {
+            string[] fromTo = edge.Split(',');
+            string nodeFrom = fromTo[0];
+            string nodeTo = fromTo[1];
+            
+            KeyValuePair<string,string> fullEdge = new KeyValuePair<string,string>(nodeFrom, nodeTo);
+            allGEdges.Add(fullEdge);
+        }
+
+        return allGEdges;
     }
 }
--- Problems/NPComplete/NPC_VERTEXCOVER/VERTEXCOVER_Controller.cs
+++ Problems/NPComplete/NPC_VERTEXCOVER/VERTEXCOVER_Controller.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using API.Problems.NPComplete.NPC_VERTEXCOVER;
+using API.Problems.NPComplete.NPC_VERTEXCOVER.Verifiers;
+using API.Problems.NPComplete.NPC_VERTEXCOVER.Solvers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -28,6 +30,8 @@ public class VERTEXCOVERGenericController : ControllerBase {
     [HttpGet]
     public String getDefault() {
         var options = new JsonSerializerOptions { WriteIndented = true };
+        // VERTEXCOVER v = new VERTEXCOVER();
+        // Console.Write(v.defaultSolver.Solve("{{a,b,c,d,e,f,g} : {(a,b) & (a,c) & (c,d) & (c,e) & (d,f) & (e,f) & (e,g)}}"));
         string jsonString = JsonSerializer.Serialize(new VERTEXCOVER(), options);
         return jsonString;
     }
@@ -41,4 +45,57 @@ public class VERTEXCOVERGenericController : ControllerBase {
 
 
 }
+
+[ApiController]
+[Route("[controller]")]
+public class VCVerifierController : ControllerBase {
+
+    [HttpGet]
+    public String getInstance([FromQuery]string certificate, [FromQuery]string problemInstance) {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        VERTEXCOVER VCProblem = new VERTEXCOVER(problemInstance);
+        VCVerifierJanita verifier = new VCVerifierJanita();
+    
+        Boolean response = verifier.Verify(VCProblem,certificate);
+        // Send back to API user
+        string jsonString = JsonSerializer.Serialize(response.ToString(), options);
+        return jsonString;
+    }
+
+}
+
+[ApiController]
+[Route("[controller]")]
+public class testInstanceController : ControllerBase {
+
+    [HttpGet]
+    public String getSingleInstance([FromQuery]string certificate, [FromQuery]string problemInstance) {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        
+        string returnString = certificate + problemInstance;
+        return returnString;
+    }
+    
+}
+
+
+[ApiController]
+[Route("[controller]")]
+public class VCSolverController : ControllerBase {
+
+      [HttpGet]
+    public String getInstance([FromQuery]string problemInstance) {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        //VERTEXCOVER VCProblem = new VERTEXCOVER(problemInstance);
+        VCSolverJanita solver = new VCSolverJanita();
+        List<KeyValuePair<string, string>> solvedInstance = solver.Solve(problemInstance);
+        //Boolean response = verifier.verify(ARCSETProblem,certificate);
+        // Send back to API user
+        string jsonString = JsonSerializer.Serialize(solvedInstance, options);
+        return jsonString;
+    }
+
+
+}
+
 
