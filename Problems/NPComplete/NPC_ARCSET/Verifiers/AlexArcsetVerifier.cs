ddadf8f36d8f3c14750f803267c4b341729a7f7a
Alex <dandiviney@isu.edu>
2022-04-28 10:06:37 -0600

    moved Reduction to VERTEXCOVER, edited descriptions of verifier, reduction, solver for Arcset

--- Problems/NPComplete/NPC_ARCSET/Verifiers/AlexArcsetVerifier.cs
+++ Problems/NPComplete/NPC_ARCSET/Verifiers/AlexArcsetVerifier.cs
@@ -8,8 +8,10 @@ class AlexArcsetVerifier : IVerifier {
 
     // --- Fields ---
     private string _verifierName = "ARCSET Verifier";
-    private string _verifierDefinition = "This is a verifier for ARCSET";
-    private string _source = " ";
+    private string _verifierDefinition =  @"This Verifier takes in an arcset problem and a list of edges to remove from that problem. It removes those edges and then checks if the problem is still an instance of ARCSET
+                                            ie. Does this input graph continue to have cycles after these input edges are removed? Returns true or false ";
+    
+    private string _source = "This verifier is essentially common knowledge, as it utilizes a widely recognized algorithm in computer science: The Depth First Search.";
 
     // --- Properties ---
     public string verifierName {
--- Problems/NPComplete/NPC_VERTEXCOVER/ReduceTo/NPC_ARCSET/NCOV_TO_ARCSETReduction.cs
+++ Problems/NPComplete/NPC_VERTEXCOVER/ReduceTo/NPC_ARCSET/NCOV_TO_ARCSETReduction.cs
@@ -3,14 +3,22 @@ using API.Problems.NPComplete.NPC_ARCSET;
 using API.Problems.NPComplete.NPC_VERTEXCOVER;
 
 
-namespace API.Problems.NPComplete.NPC_ExactCover.ReduceTo.NPC_ARCSET;
+namespace API.Problems.NPComplete.NPC_VERTEXCOVER.ReduceTo.NPC_ARCSET;
 
 class NCOV_TO_ARCSETReduction : IReduction<VERTEXCOVER, ARCSET> {
 
   
 
     // --- Fields ---
-    private string _reductionDefinition = "This Reduction is an implementation of Lawler and Karp's reduction as laid out in Karp's 21 NP_Complete Problems.";
+    private string _reductionDefinition = @"This Reduction is an implementation of Lawler and Karp's reduction as laid out in Karp's 21 NP_Complete Problems. 
+                                            It takes an instance of an undirected graph (specifically an instance of VERTEXCOVER) and returns an instance of ARCSET (ie. a Directed Graph)
+                                            Specifically, a reduction follows the following algorithm: 
+                                            For an undirected graph H: Where H is made up of <V,E>
+                                            Convert the undirected edges in E to pairs of directed edges. So an undirected edge {{A,B}} turns into the directed pair of edges {(A,B),(B,A)} 
+                                            Then turn every node into a pair of nodes denoted by 0 and 1. So a node 'A' turns into the two nodes '<A,0>' and '<A,1>'
+                                            Now looks at the pairs of edges in E and maps from 1 to 0. So an edge (A,B) turns into (<A,1>, <B,0>) and edge (B,A) becomes (<B,1>,<A,0>)
+                                            Then add directed edges from every 0 node 'u' to 1 node 'u'. ie. creates edges from <A,0> to <A,1>, <B,0> to <B,1> … <Z,0> to <Z,1>
+                                            Now the algortithm has created an ARCSET instance (in other words, a Digraph). ";
     private string _source = "http://cgi.di.uoa.gr/~sgk/teaching/grad/handouts/karp.pdf"; //Alex NOTE: Change later to real citation.
     private VERTEXCOVER _reductionFrom;
     private ARCSET _reductionTo;
