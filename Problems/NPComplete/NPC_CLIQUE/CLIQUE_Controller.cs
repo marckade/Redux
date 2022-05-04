e866380d5112f076aeff6bed7aee4d654c9e8532
aamijani <55214847+aamijani@users.noreply.github.com>
2022-05-03 18:56:05 -0600

    Updated VC

--- Problems/NPComplete/NPC_CLIQUE/CLIQUE_Controller.cs
+++ Problems/NPComplete/NPC_CLIQUE/CLIQUE_Controller.cs
@@ -64,7 +64,7 @@ public class sipserReduceToVCController : ControllerBase {
         //SAT3 defaultSAT3 = new SAT3(problemInstance);
         //SipserReduction reduction = new SipserReduction(defaultSAT3);
         sipserReduction reduction = new sipserReduction(defaultCLIQUE);
-        string jsonString = JsonSerializer.Serialize(reduction, options);
+        string jsonString = JsonSerializer.Serialize(reduction.reductionTo, options);
         return jsonString;
     }
 
