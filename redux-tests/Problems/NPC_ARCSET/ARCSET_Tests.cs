c9e19f56a4c8d4d9ce682080c3e47acb609016f4
Alex <dandiviney@isu.edu>
2022-07-01 20:03:26 -0600

    Added xunit testing

---
+++ redux-tests/Problems/NPC_ARCSET/ARCSET_Tests.cs
@@ -0,0 +1,14 @@
+
+
+using Xunit;
+
+namespace redux_tests;
+
+public class ARCSET_Tests
+{
+    [Fact]
+    public void ARCSET_Generic_Add_Test()
+    {
+        Assert.Equal(2, (1 + 1));
+    }
+}
---
+++ redux-tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace redux_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(3, (1 + 2));
+    }
+}
