55c6fa3108643986489b00df07d6670ca6d5ed44
eardcale <eardcale@isu.edu>
2023-01-20 11:57:53 -0700

    Exact Cover Problem finished

--- Problems/NPComplete/NPC_ExactCover/ExactCover_Class.cs
+++ Problems/NPComplete/NPC_ExactCover/ExactCover_Class.cs
@@ -4,20 +4,24 @@ using API.Problems.NPComplete.NPC_ExactCover.Verifiers;
 
 namespace API.Problems.NPComplete.NPC_ExactCover;
 
-class ExactCover : IProblem<GenericSolver,GenericVerifier> {
+class ExactCover : IProblem<ExactCoverBruteForce,ExactCoverVerifier> {
 
     // --- Fields ---
     private string _problemName = "Exact Cover";
-    private string _formalDefinition = "Exact Cover = {<S, X, S*> | S is a collection of subsets of a set X and S* is a subcollection, exact cover, of S} ";
+    private string _formalDefinition = "Exact Cover = {<S, X> | S is a collection of subsets of a set X where S* exists such that S* is a subcollection of S and an exact cover, of S. This means that each element of X is in exactly one subset of S*.} ";
     private string _problemDefinition = "The exact cover problem is a decision problem to determine if an exact cover exists for some <S, X>";
     private string _source = "Karp, Richard M. Reducibility among combinatorial problems. Complexity of computer computations. Springer, Boston, MA, 1972. 85-103.";
-    private string[] _contributers = { "Author Unknown", "Alex Diviney" };
+    private string[] _contributers = { "Caleb Eardley", "Alex Diviney" };
 
     
-    private string _defaultInstance = "{{ (), (1 & 3), (2 & 3), (2 & 4)} : {1,2,3,4} : {(1 & 3), (2 & 4)}}";
+    private string _defaultInstance = "{{1,2,3},{2,3},{4,1} : {1,2,3,4}}";
+    private string _instance = string.Empty;
+
     private string _wikiName = "";
-    private GenericSolver _defaultSolver = new GenericSolver();
-    private GenericVerifier _defaultVerifier = new GenericVerifier();
+    private ExactCoverBruteForce _defaultSolver = new ExactCoverBruteForce();
+    private ExactCoverVerifier _defaultVerifier = new ExactCoverVerifier();
+    List<List<string>> _S = new List<List<string>>();
+    List<string> _X = new List<string>();
 
     // --- Properties ---
     public string problemName {
@@ -56,20 +60,67 @@ class ExactCover : IProblem<GenericSolver,GenericVerifier> {
             return _defaultInstance;
         }
     }
-    public GenericSolver defaultSolver {
+    public ExactCoverBruteForce defaultSolver {
         get {
             return _defaultSolver;
         }
     }
-    public GenericVerifier defaultVerifier {
+    public ExactCoverVerifier defaultVerifier {
         get {
             return _defaultVerifier;
         }
     }
-    // --- Methods Including Constructors ---
-    public ExactCover() {
+    public string instance {
+        get {
+            return _instance;
+        }
+        set {
+            _instance = value;
+        }
     }
-    public ExactCover(string GInput) {
+    public List<List<string>> S {
+        get {
+            return _S;
+        }
+        set{
+            _S = value;
+        }
+    }
+
+    public List<string> X {
+        get{
+            return _X;
+        }
+        set{
+            _X = value;
+        }
+    }
+    // --- Methods Including Constructors ---
+
+    private List<List<string>> GetS(string instance){
+        List<List<string>> S = new List<List<string>>();
+        List<string> S_stringList = instance.Replace(" ","").Split(":")[0].Split("},{").ToList();
+        foreach(string stringSet in S_stringList){
+            List<string> subset = stringSet.Replace("{","").Replace("}","").Split(",").ToList();
+            S.Add(subset);
+        }
+        return S;
+
+
+    }
+    private List<string> GetX(string instance){
+        List<string> X = instance.Split(":")[1].Replace("{","").Replace("}","").Replace(" ","").Split(",").ToList();
+        return X;
+    }
+    public ExactCover() {
+        _instance = _defaultInstance;
+        _S = GetS(_instance);
+        _X = GetX(_instance);
+    }
+    public ExactCover(string instance) {
+         _instance = instance;
+        _S = GetS(_instance);
+        _X = GetX(_instance);
     }
 
 
---
+++ Problems/NPComplete/NPC_ExactCover/Solvers/ExactCoverBruteForce.cs
@@ -0,0 +1,79 @@
+using API.Interfaces;
+
+namespace API.Problems.NPComplete.NPC_ExactCover.Solvers;
+class ExactCoverBruteForce : ISolver {
+
+    // --- Fields ---
+    private string _solverName = "Exact Cover Brute Force Solver";
+    private string _solverDefinition = "This is a generic brute force solver for Exact Cover";
+    private string _source = "This person Caleb Eardley";
+    private string[] _contributers = { "Caleb Eardley"};
+
+
+    // --- Properties ---
+    public string solverName {
+        get {
+            return _solverName;
+        }
+    }
+    public string solverDefinition {
+        get {
+            return _solverDefinition;
+        }
+    }
+    public string source {
+        get {
+            return _source;
+        }
+    }
+    public string[] contributers{
+        get{
+            return _contributers;
+        }
+    }
+    // --- Methods Including Constructors ---
+    public ExactCoverBruteForce() {
+        
+    }
+    private string BinaryToCertificate(List<int> binary,  List<List<string>> S ){
+        string certificate = "";
+        for(int i = 0; i< binary.Count; i++){
+            if(binary[i] == 1){
+                certificate += "{";
+                foreach(var element in S[i]){
+                    certificate += element+",";
+                }
+                certificate = certificate.TrimEnd(',') + "},";
+            }
+        }
+        return "{" + certificate.TrimEnd(',') + "}";
+
+    }
+    private void nextBinary(List<int> binary){
+        for(int i = 0; i< binary.Count; i++){
+            if(binary[i] == 0){
+                binary[i] += 1;
+                return;
+            }
+            else if(binary[i] == 1){
+                binary[i] = 0;
+            }
+        }
+    }
+       
+    public string solve(ExactCover exactCover){
+        List<int> binary = new List<int>(){};
+        for(int i = 0; i < exactCover.S.Count; i++){
+            binary.Add(0);
+        }
+        string certificate = BinaryToCertificate(binary, exactCover.S);
+        for(int i=0; i< Math.Pow(2,binary.Count); i++){
+            nextBinary(binary);
+            certificate = BinaryToCertificate(binary, exactCover.S);
+            if(exactCover.defaultVerifier.verify(exactCover, certificate)){
+                return certificate;
+            }
+        }
+        return "{}";
+    }
+}
